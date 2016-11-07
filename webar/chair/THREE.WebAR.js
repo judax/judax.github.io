ced88e67ea62c833f1fdd73e9c79d17397cdf050
ijamardo <ijamardo@ijamardo-glaptop.roam.corp.google.com>
2016-11-07 20:18:20 +0100

    New orientations.

--- webar/chair/THREE.WebAR.js
+++ webar/chair/THREE.WebAR.js
@@ -278,8 +278,8 @@ THREE.WebAR._worldIn = new THREE.Vector3(0.0, 0.0, 1.0);
 THREE.WebAR._orientationCorrectionQuaternions = [
 	new THREE.Quaternion().setFromAxisAngle(THREE.WebAR._worldIn, 0),
 	new THREE.Quaternion().setFromAxisAngle(THREE.WebAR._worldIn, THREE.Math.degToRad(90)),
-	new THREE.Quaternion().setFromAxisAngle(THREE.WebAR._worldIn, THREE.Math.degToRad(180)),
-	new THREE.Quaternion().setFromAxisAngle(THREE.WebAR._worldIn, THREE.Math.degToRad(270))
+	new THREE.Quaternion().setFromAxisAngle(THREE.WebAR._worldIn, THREE.Math.degToRad(270)),
+	new THREE.Quaternion().setFromAxisAngle(THREE.WebAR._worldIn, THREE.Math.degToRad(180))
 ];
 
 
