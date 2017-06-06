f9ad57b568d8f41de06cfac054ae1cfc0d296c13
ijamardo <ijamardo@ijamardo-glaptop2.roam.corp.google.com>
2017-06-06 16:55:46 -0700

    The THREE.WebAR library now parametrizes the transformPoints argument and the occlusion example provides a way to test it.

--- webar/examples/libs/THREE.WebAR.js
+++ webar/examples/libs/THREE.WebAR.js
@@ -101,11 +101,11 @@ THREE.WebAR.VRPointCloud.prototype.getBufferGeometry = function() {
 * @param {boolean} updateBufferGeometry A flag to indicate if the underlying THREE.BufferGeometry should also be updated. Updating the THREE.BufferGeometry is very cost innefficient so it is better to only do it if necessary (only if the buffer geometry is going to be rendered for example). If this flag is set to false,  then the underlying point cloud is updated but not buffer geometry that represents it. Updating the point cloud is important to be able to call functions that operate with it, like the getPickingPointAndPlaneInPointCloud function.
 * @param {number} pointsToSkip A positive integer from 0-N that specifies the number of points to skip when returning the point cloud. If the updateBufferGeometry flag is activated (true) then this parameter allows to specify the density of the point cloud. A values of 0 means all the detected points need to be returned. A number of 1 means that 1 every other point needs to be skipped and thus, half of the detected points will be retrieved, and so on. If the parameter is not specified, 0 is considered.
 */
-THREE.WebAR.VRPointCloud.prototype.update = function(updateBufferGeometry, pointsToSkip) {
+THREE.WebAR.VRPointCloud.prototype.update = function(updateBufferGeometry, pointsToSkip, transformPoints) {
   if (!this._vrDisplay) return;
   this._vrDisplay.getPointCloud(this._pointCloud, 
     !updateBufferGeometry, typeof(pointsToSkip) === "number" ? 
-      pointsToSkip : 0, true);
+      pointsToSkip : 0, !!transformPoints);
   if (!updateBufferGeometry) return;
   if (this._pointCloud.numberOfPoints > 0) {
     this._positions.needsUpdate = true;
